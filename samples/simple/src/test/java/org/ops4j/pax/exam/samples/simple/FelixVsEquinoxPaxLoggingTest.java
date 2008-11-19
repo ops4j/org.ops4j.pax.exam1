a243d668da810411dc315c3d4884cb9fa95198f8
tonit <tonit@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-11-19 17:03:22 +0000

    added docs (license), cleaned some imports, reformat.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/exam@13401 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- samples/simple/src/test/java/org/ops4j/pax/exam/samples/simple/FelixVsEquinoxPaxLoggingTest.java
+++ samples/simple/src/test/java/org/ops4j/pax/exam/samples/simple/FelixVsEquinoxPaxLoggingTest.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2008 Toni Menzel
+ *
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ops4j.pax.exam.samples.simple;
 
 import org.osgi.framework.ServiceReference;
@@ -5,8 +22,8 @@ import org.osgi.service.log.LogService;
 import org.ops4j.pax.exam.api.TestRunnerConnector;
 import static org.ops4j.pax.exam.connector.paxrunner.GenericConnector.*;
 import org.ops4j.pax.exam.connector.paxrunner.Platforms;
-import org.ops4j.pax.exam.junit.MultiConfigOsgiTestCase;
 import org.ops4j.pax.exam.junit.JunitSupport;
+import org.ops4j.pax.exam.junit.MultiConfigOsgiTestCase;
 
 /**
  * This test compares felix and equinox directly in terms of compatibilty with pax logging support.
@@ -14,26 +31,29 @@ import org.ops4j.pax.exam.junit.JunitSupport;
  * @author Toni Menzel (tonit)
  * @since Nov 11, 2008
  */
-public class FelixVsEquinoxPaxLoggingTest extends MultiConfigOsgiTestCase {
+public class FelixVsEquinoxPaxLoggingTest extends MultiConfigOsgiTestCase
+{
 
-    protected TestRunnerConnector[] configure() {
+    protected TestRunnerConnector[] configure()
+    {
         return new TestRunnerConnector[]{
-                create(createBundleProvision()
-                        .addBundle("mvn:org.ops4j.pax.logging/pax-logging-api")
-                        .addBundle("mvn:org.ops4j.pax.logging/pax-logging-service")
-                        .addBundle(JunitSupport.bundles())
-                ).setPlatform(Platforms.FELIX),
-                create(createBundleProvision()
-                        .addBundle("mvn:org.ops4j.pax.logging/pax-logging-api")
-                        .addBundle("mvn:org.ops4j.pax.logging/pax-logging-service")
-                        .addBundle(JunitSupport.bundles())
-                ).setPlatform(Platforms.EQUINOX)
+            create( createBundleProvision()
+                .addBundle( "mvn:org.ops4j.pax.logging/pax-logging-api" )
+                .addBundle( "mvn:org.ops4j.pax.logging/pax-logging-service" )
+                .addBundle( JunitSupport.bundles() )
+            ).setPlatform( Platforms.FELIX ),
+            create( createBundleProvision()
+                .addBundle( "mvn:org.ops4j.pax.logging/pax-logging-api" )
+                .addBundle( "mvn:org.ops4j.pax.logging/pax-logging-service" )
+                .addBundle( JunitSupport.bundles() )
+            ).setPlatform( Platforms.EQUINOX )
         };
     }
 
-    public void testServiceExposed() {
-        ServiceReference ref = bundleContext.getServiceReference(LogService.class.getName());
-        assertNotNull("LogService should be exposed", ref);
+    public void testServiceExposed()
+    {
+        ServiceReference ref = bundleContext.getServiceReference( LogService.class.getName() );
+        assertNotNull( "LogService should be exposed", ref );
 
     }
 
--- samples/simple/src/test/java/org/ops4j/pax/exam/samples/simple/JUnit4Test.java
+++ samples/simple/src/test/java/org/ops4j/pax/exam/samples/simple/JUnit4Test.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2008 Toni Menzel
+ *
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ops4j.pax.exam.samples.simple;
 
 import static org.junit.Assert.*;
@@ -17,29 +34,34 @@ import org.ops4j.pax.exam.junit.JunitSupport;
  * @author Toni Menzel (tonit)
  * @since Oct 14, 2008
  */
-@RunWith(OsgiTestRunner.class)
-public class JUnit4Test {
+@RunWith( OsgiTestRunner.class )
+public class JUnit4Test
+{
 
     @Configuration
-    public TestRunnerConnector configure() {
-        return create(createBundleProvision().addBundle(JunitSupport.bundles())).setPlatform(Platforms.FELIX);
+    public TestRunnerConnector configure()
+    {
+        return create( createBundleProvision().addBundle( JunitSupport.bundles() ) ).setPlatform( Platforms.FELIX );
     }
 
     @Test
-    public void aVerySimpleOne() {
-        System.out.println("Hello World");
+    public void aVerySimpleOne()
+    {
+        System.out.println( "Hello World" );
     }
 
     @Test
-    public void anotherTestForTesting() {
-        System.out.println("Again, Hello World");
+    public void anotherTestForTesting()
+    {
+        System.out.println( "Again, Hello World" );
         //  fail( "thats bad" );
     }
 
     @Test
-    public void haveBundleContextPassedInto(BundleContext bc) {
-        System.out.println("Again, Hello World");
-        assertNotNull(bc);
+    public void haveBundleContextPassedInto( BundleContext bc )
+    {
+        System.out.println( "Again, Hello World" );
+        assertNotNull( bc );
     }
 
 
