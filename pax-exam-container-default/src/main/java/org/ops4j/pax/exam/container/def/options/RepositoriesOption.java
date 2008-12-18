0622a5b6628a8195e31ce59c9bca46118077076c
tonit <tonit@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-12-18 23:28:00 +0000

    PAXEXAM-3 added javadoc
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/exam@13803 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- pax-exam-container-default/src/main/java/org/ops4j/pax/exam/container/def/options/RepositoriesOption.java
+++ pax-exam-container-default/src/main/java/org/ops4j/pax/exam/container/def/options/RepositoriesOption.java
@@ -1,9 +1,28 @@
+/*
+ * Copyright 2008 Toni Menzel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ops4j.pax.exam.container.def.options;
 
 import org.ops4j.pax.exam.Option;
 import static org.ops4j.lang.NullArgumentException.*;
 
 /**
+ * The option specifiying pax runners repository option.
+ *
  * @author Toni Menzel (tonit)
  * @since Dec 18, 2008
  */
@@ -40,6 +59,10 @@ public class RepositoriesOption implements Option
         return this;
     }
 
+    /**
+     *
+     * @return the full repository as given plus eventual snapshot/release tags.
+     */
     public String getRepositoryAsOption()
     {
 
