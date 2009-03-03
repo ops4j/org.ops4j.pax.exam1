7ea3ad8fdd165224e6f2890db86e725258bbe616
tonit <tonit@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-03-03 13:43:11 +0000

    separate trace project to be used to provide meaningful dumps of problematic environments.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/exam@14326 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- pax-exam-it-02/src/test/java/org/ops4j/pax/exam/it/DefaultTrace.java
+++ pax-exam-it-02/src/test/java/org/ops4j/pax/exam/it/DefaultTrace.java
@@ -1,20 +1,13 @@
 package org.ops4j.pax.exam.it;
 
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.Bundle;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
 import org.ops4j.pax.exam.Inject;
-import org.ops4j.pax.exam.Option;
-import static org.ops4j.pax.exam.CoreOptions.*;
-import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.*;
-import org.ops4j.pax.exam.junit.Configuration;
-import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 
 /**
- *
  * This is a sample that is being configured to give max. insight into pax exam by
  * - having log service installed
  * - setting low log levels.
@@ -24,23 +17,13 @@ import org.ops4j.pax.exam.junit.JUnit4TestRunner;
  * @author Toni Menzel (tonit)
  * @since Mar 3, 2009
  */
-@RunWith( JUnit4TestRunner.class )
-public class MinimumTrace
+public abstract class DefaultTrace
 {
 
     @Inject
     BundleContext bundleContext = null;
 
-    public Log logger = LogFactory.getLog( MinimumTrace.class );
-
-    @Configuration
-    public static Option[] configure()
-    {
-        return options(
-            logProfile(),
-            systemProperty( "org.ops4j.pax.logging.DefaultServiceLog.level" ).value( "DEBUG" )
-        );
-    }
+    public Log logger = LogFactory.getLog( DefaultTrace.class );
 
     @Test
     public void test1()
---
+++ pax-exam-it-02/src/test/java/org/ops4j/pax/exam/it/TraceFelix.java
@@ -0,0 +1,27 @@
+package org.ops4j.pax.exam.it;
+
+import org.junit.runner.RunWith;
+import static org.ops4j.pax.exam.CoreOptions.*;
+import org.ops4j.pax.exam.Option;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.*;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+
+/**
+ * @author Toni Menzel (tonit)
+ * @since Mar 3, 2009
+ */
+@RunWith( JUnit4TestRunner.class )
+public class TraceFelix extends DefaultTrace
+{
+
+    @Configuration
+    public static Option[] configure()
+    {
+        return options(
+            logProfile(),
+            systemProperty( "org.ops4j.pax.logging.DefaultServiceLog.level" ).value( "DEBUG" ),
+            felix()
+        );
+    }
+}
