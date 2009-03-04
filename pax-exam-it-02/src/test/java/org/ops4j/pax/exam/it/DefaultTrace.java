2b5e9691feb56a2f14f27f333db71b3697f1fc1e
tonit <tonit@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-03-04 08:20:13 +0000

    no static config required.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/exam@14332 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- pax-exam-it-02/src/test/java/org/ops4j/pax/exam/it/DefaultTrace.java
+++ pax-exam-it-02/src/test/java/org/ops4j/pax/exam/it/DefaultTrace.java
@@ -1,8 +1,27 @@
+/*
+ * Copyright 2009 Toni Menzel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ops4j.pax.exam.it;
 
+import java.util.Properties;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
+import org.junit.Before;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.ops4j.pax.exam.Inject;
@@ -37,7 +56,7 @@ public abstract class DefaultTrace
         logger.info( "This is running inside Felix. With all configuration set up like you specified. " );
         for( Bundle b : bundleContext.getBundles() )
         {
-            logger.info( "Bundle " + b.getBundleId() + " : " + b.getSymbolicName() );
+            System.out.println( "--> Bundle " + b.getBundleId() + " : " + b.getSymbolicName()  + " : " + b.getState() );
         }
 
     }
