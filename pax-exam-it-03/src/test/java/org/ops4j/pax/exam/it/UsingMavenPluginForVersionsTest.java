59204383613862e70ccb874cb83c47df185276a6
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-03-09 15:20:45 +0000

    PAXEXAM-21 Intergration test/example for hw to automatically determine maven artifact version.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/exam@14454 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ pax-exam-it-03/src/test/java/org/ops4j/pax/exam/it/UsingMavenPluginForVersionsTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2008 Alin Dreghiciu
+ *
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.exam.it;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.ops4j.pax.exam.CoreOptions.*;
+import static org.ops4j.pax.exam.MavenUtils.*;
+import org.ops4j.pax.exam.Option;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.*;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+
+/**
+ * How to use the maven plaugin for determining maven artifact version integration tests.
+ *
+ * @author Alin Dreghiciu (adreghiciu@gmail.com)
+ * @since 0.3.0, November 18, 2008
+ */
+@RunWith( JUnit4TestRunner.class )
+public class UsingMavenPluginForVersionsTest
+{
+
+    /**
+     * Pax Exam test options that provisions the Pax URL mvn: url handler bundle.
+     *
+     * @return integration tests options
+     */
+    @Configuration
+    public static Option[] configureForValidURL()
+    {
+        return options(
+            provision(
+                mavenBundle().groupId( "org.ops4j.pax.url" ).artifactId( "pax-url-mvn" ).version( asInProject() )
+            ),
+            logProfile()
+        );
+    }
+
+    /**
+     * Tests that the Pax URL mvn: url handler has been provisioned by creating a mvn: url. If the url creation fails,
+     * it means that the bundle was not provisioned.
+     *
+     * @throws MalformedURLException - Not expected
+     */
+    @Test
+    public void validURL()
+        throws MalformedURLException
+    {
+        new URL( "mvn:org.ops4j.pax.swissbox/pax-swissbox-core" );
+    }
+
+}
