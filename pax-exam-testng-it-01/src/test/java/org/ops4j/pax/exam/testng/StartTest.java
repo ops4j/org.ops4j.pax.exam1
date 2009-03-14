d528da597f6060ea8a8de98002a4a95b19125c56
tonit <tonit@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-03-14 17:15:54 +0000

    PAXEXAM-28 initial commit (bootstrapping projects and dependencies)
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/exam@14518 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ pax-exam-testng-it-01/src/test/java/org/ops4j/pax/exam/testng/StartTest.java
@@ -0,0 +1,17 @@
+package org.ops4j.pax.exam.testng;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author Toni Menzel (tonit)
+ * @since Mar 14, 2009
+ */
+public class StartTest
+{
+
+    @Test
+    public void use()
+    {
+
+    }
+}
