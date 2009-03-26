6d6519344c426b008e92c70283803dde3d995d23
tonit <tonit@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-03-26 08:33:12 +0000

    test
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/exam@14635 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ pax-exam-it-05/testproject/src/test/java/org/ops4j/pax/exam/testproject/DependencyTest.java
@@ -0,0 +1,41 @@
+package org.ops4j.pax.exam.testproject;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.ops4j.pax.exam.junit.MavenConfiguredJUnit4TestRunner;
+import org.ops4j.pax.exam.Inject;
+
+/**
+ * @author Toni Menzel (tonit)
+ * @since Mar 26, 2009
+ */
+@RunWith( org.ops4j.pax.exam.junit.MavenConfiguredJUnit4TestRunner.class )
+public class DependencyTest
+{
+
+    @Inject
+    BundleContext context;
+
+    @Test
+    public void usePlugin()
+    {
+        // this is a fragment that should be one of the "api" that come out of PAXEXAM-23.
+        // "simple check for bundle states, wait for dependencies to resolve,unresolve" and so on.
+
+        boolean foundAndActive = false;
+        for( Bundle b : context.getBundles() )
+        {
+            if( b.getSymbolicName().equals( "Test Bundle Dependency" ) )
+            {
+                if( b.getState() == Bundle.ACTIVE )
+                {
+                    foundAndActive = true;
+                }
+            }
+        }
+        assertTrue( foundAndActive );
+    }
+}
