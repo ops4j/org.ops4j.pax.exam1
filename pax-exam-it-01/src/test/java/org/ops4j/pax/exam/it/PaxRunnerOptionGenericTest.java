95ee326c0665cb711baab10f4c79ded2946f0ff6
tonit <tonit@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-04-21 11:30:55 +0000

    PAXEXAM-62 and PAXEXAM-64 tests for new options.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/exam@14972 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ pax-exam-it-01/src/test/java/org/ops4j/pax/exam/it/PaxRunnerOptionGenericTest.java
@@ -0,0 +1,37 @@
+package org.ops4j.pax.exam.it;
+
+import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.Option;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.*;
+import static org.ops4j.pax.exam.CoreOptions.*;
+
+/**
+ * @author Toni Menzel (tonit)
+ * @since Apr 21, 2009
+ */
+@RunWith( JUnit4TestRunner.class )
+public class PaxRunnerOptionGenericTest
+{
+
+    @Configuration
+    public static Option[] configure()
+    {
+
+        return options(
+            // get from env:
+            paxrunner( "platform", "equinox" ),
+            paxrunner( "profiles", "web" )
+
+        );
+    }
+
+    @Test
+    public void use()
+    {
+
+    }
+
+}
