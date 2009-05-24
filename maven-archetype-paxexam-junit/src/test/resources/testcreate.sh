6011b84a1a5a7f55ec344efd5039518e035a113b
tonit <tonit@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-05-24 14:41:46 +0000

    updated unreleased poms to 0.7.0-SNAPSHOT
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/exam@15290 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- maven-archetype-paxexam-junit/src/test/resources/testcreate.sh
+++ maven-archetype-paxexam-junit/src/test/resources/testcreate.sh
@@ -1,1 +1,1 @@
-mvn archetype:create -DarchetypeGroupId=org.ops4j.pax.exam -DarchetypeArtifactId=maven-archetype-paxexam-junit -DarchetypeVersion=0.6.0-SNAPSHOT -DgroupId=com.company -DartifactId=company-osgitests
+mvn archetype:create -DarchetypeGroupId=org.ops4j.pax.exam -DarchetypeArtifactId=maven-archetype-paxexam-junit -DarchetypeVersion=0.7.0-SNAPSHOT -DgroupId=com.company -DartifactId=company-osgitests
