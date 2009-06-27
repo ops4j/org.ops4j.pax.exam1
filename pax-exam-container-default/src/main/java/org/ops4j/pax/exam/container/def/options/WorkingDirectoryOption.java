d095d4a7a3ebc5f7e1a7bb8179d792716356c135
tonit <tonit@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-06-27 15:12:38 +0000

    PAXEXAM-106 proper formatting and code docs.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/exam@15561 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- pax-exam-container-default/src/main/java/org/ops4j/pax/exam/container/def/options/WorkingDirectoryOption.java
+++ pax-exam-container-default/src/main/java/org/ops4j/pax/exam/container/def/options/WorkingDirectoryOption.java
@@ -3,21 +3,23 @@ package org.ops4j.pax.exam.container.def.options;
 import org.ops4j.pax.exam.Option;
 
 /**
- * Created by IntelliJ IDEA.
- * User: tonit
- * Date: Jun 26, 2009
- * Time: 7:51:34 PM
- * To change this template use File | Settings | File Templates.
+ * Option to overwrite --workingDirectory default.
+ *
+ * @author Toni Menzel (toni@okidokiteam.com)
+ * @since 0.3.0 December 10, 2008
  */
 public class WorkingDirectoryOption implements Option
 {
+
     final private String m_workingDirectory;
 
-    public WorkingDirectoryOption(String directory) {
+    public WorkingDirectoryOption( String directory )
+    {
         m_workingDirectory = directory;
     }
 
-    public String getWorkingDirectory() {
+    public String getWorkingDirectory()
+    {
         return m_workingDirectory;
     }
 }
