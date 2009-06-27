bd7405f1786cb05d1b407c3ec255e819346ce55b
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-06-27 19:07:20 +0000

    Prepare for next version: 1.0.1
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/exam@15570 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- maven-archetype-paxexam-junit/src/test/resources/testcreate.sh
+++ maven-archetype-paxexam-junit/src/test/resources/testcreate.sh
@@ -1,1 +1,1 @@
-mvn archetype:create -DarchetypeGroupId=org.ops4j.pax.exam -DarchetypeArtifactId=maven-archetype-paxexam-junit -DarchetypeVersion=1.0.0-SNAPSHOT -DgroupId=com.company -DartifactId=company-osgitests
+mvn archetype:create -DarchetypeGroupId=org.ops4j.pax.exam -DarchetypeArtifactId=maven-archetype-paxexam-junit -DarchetypeVersion=1.0.1-SNAPSHOT -DgroupId=com.company -DartifactId=company-osgitests
