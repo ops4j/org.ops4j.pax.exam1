7df152367a64730507431b7a60daf371487ef730
tonit <tonit@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-09-01 08:07:10 +0000

    Upgraded unreleased artifacts.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/exam@16025 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- maven-archetype-paxexam-junit/src/test/resources/testcreate.sh
+++ maven-archetype-paxexam-junit/src/test/resources/testcreate.sh
@@ -1,1 +1,1 @@
-mvn archetype:create -DarchetypeGroupId=org.ops4j.pax.exam -DarchetypeArtifactId=maven-archetype-paxexam-junit -DarchetypeVersion=1.1.0-SNAPSHOT -DgroupId=com.company -DartifactId=company-osgitests
+mvn archetype:create -DarchetypeGroupId=org.ops4j.pax.exam -DarchetypeArtifactId=maven-archetype-paxexam-junit -DarchetypeVersion=1.1.1-SNAPSHOT -DgroupId=com.company -DartifactId=company-osgitests
