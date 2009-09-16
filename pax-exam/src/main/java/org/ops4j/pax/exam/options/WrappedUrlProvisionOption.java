bbf244acf8a57fb9fa817e62f3f217ba8d8c4df9
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-09-16 21:38:41 +0000

    [PAXEXAM-126] wrapBundle() option should support setting of BND instructions
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/exam@16207 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- pax-exam/src/main/java/org/ops4j/pax/exam/options/WrappedUrlProvisionOption.java
+++ pax-exam/src/main/java/org/ops4j/pax/exam/options/WrappedUrlProvisionOption.java
@@ -28,6 +28,24 @@ public class WrappedUrlProvisionOption
 {
 
     /**
+     * Wrapped jar bundle symbolic name. Can be null.
+     */
+    private String m_bundleSymbolicName;
+    /**
+     * Wrapped jar bundle version. Can be null.
+     */
+    private String m_bundleVersion;
+    /**
+     * Wrapped jar imports.
+     */
+    private String[] m_imports;
+    /**
+     * Wrapped jar exports.
+     */
+    private String[] m_exports;
+    private String[] m_instructions;
+
+    /**
      * Constructor.
      *
      * @param url wrapped jar url (cannot be null or empty)
@@ -56,7 +74,131 @@ public class WrappedUrlProvisionOption
      */
     public String getURL()
     {
-        return "wrap:" + super.getURL();
+        final StringBuilder options = new StringBuilder();
+        if( m_bundleSymbolicName != null )
+        {
+            options.append( "Bundle-SymboliName=" ).append( m_bundleSymbolicName );
+        }
+        if( m_bundleVersion != null )
+        {
+            if( options.length() > 0 )
+            {
+                options.append( "&" );
+            }
+            options.append( "Bundle-Version=" ).append( m_bundleVersion );
+        }
+        if( m_imports != null && m_imports.length > 0 )
+        {
+            if( options.length() > 0 )
+            {
+                options.append( "&" );
+            }
+            options.append( "Import-Package=" );
+            for( String entry : m_imports )
+            {
+                options.append( entry ).append( "," );
+            }
+            options.delete( options.length(), options.length() + 1 );
+        }
+        if( m_exports != null && m_exports.length > 0 )
+        {
+            if( options.length() > 0 )
+            {
+                options.append( "&" );
+            }
+            options.append( "Export-Package=" );
+            for( String entry : m_exports )
+            {
+                options.append( entry ).append( "," );
+            }
+            options.delete( options.length(), options.length() + 1 );
+        }
+        if( m_instructions != null && m_instructions.length > 0 )
+        {
+            for( String entry : m_instructions )
+            {
+                if( options.length() > 0 )
+                {
+                    options.append( "&" );
+                }
+                options.append( entry );
+            }
+        }
+        if( options.length() > 0 )
+        {
+            options.insert( 0, "$" );
+        }
+        return "wrap:" + super.getURL() + options.toString();
+    }
+
+    /**
+     * Sets wrapped jar bundle symbolic name.
+     *
+     * @param bundleSymbolicName bundle symbolic name
+     *
+     * @return itself
+     */
+    public WrappedUrlProvisionOption bundleSymbolicName( final String bundleSymbolicName )
+    {
+        m_bundleSymbolicName = bundleSymbolicName;
+
+        return this;
+    }
+
+    /**
+     * Sets wrapped jar bundle version.
+     *
+     * @param bundleVersion bundle symbolic name
+     *
+     * @return itself
+     */
+    public WrappedUrlProvisionOption bundleVersion( final String bundleVersion )
+    {
+        m_bundleVersion = bundleVersion;
+
+        return this;
+    }
+
+    /**
+     * Sets wrapped jar imports.
+     *
+     * @param imports BND style imports
+     *
+     * @return itself
+     */
+    public WrappedUrlProvisionOption imports( final String... imports )
+    {
+        m_imports = imports;
+
+        return this;
+    }
+
+    /**
+     * Sets wrapped jar exports.
+     *
+     * @param exports BND style exports
+     *
+     * @return itself
+     */
+    public WrappedUrlProvisionOption exports( final String... exports )
+    {
+        m_exports = exports;
+
+        return this;
+    }
+
+    /**
+     * Sets wrapped jar raw BND instructions.
+     *
+     * @param instructions BND instructions
+     *
+     * @return itself
+     */
+    public WrappedUrlProvisionOption instructions( final String... instructions )
+    {
+        m_instructions = instructions;
+
+        return this;
     }
 
     /**
